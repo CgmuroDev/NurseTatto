b82d6d3f9bde7f1fc6efb3ec8c87839bf1506806
carlos muro <gonzalezmurocarlosmanuel@gmail.com>
2026-05-04 13:43:41 -0600

    Refactor image source paths and update configuration for base URL

--- nurse-tatto/astro.config.mjs
+++ nurse-tatto/astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://cgmurodev.github.io',
+  base: '/NurseTatto',
   integrations: [tailwind()]
 });
